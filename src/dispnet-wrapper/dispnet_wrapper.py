78c08c1d45cc2c156471121d808c8ac60021fe46
Tyler Kurtz <tylerkurtz8@gmail.com>
2019-06-27 14:28:45 -0700

    Moved pub sub into main

--- src/dispnet-wrapper/dispnet_wrapper.py
+++ src/dispnet-wrapper/dispnet_wrapper.py
@@ -11,11 +11,6 @@ class DispnetWrapper:
         c = nd.load_module('/home/netdef/netdef_models/DispNet3/CSS/controller.py').Controller()
         self.disp_net = c.net_actions(net_dir=c.base_path)
 
-        left_image_sub = message_filters.Subscriber('/stereo/left/image_raw', Image)
-        right_image_sub = message_filters.Subscriber('/stereo/right/image_raw', Image)
-        time_sync = message_filters.TimeSynchronizer([self, left_image_sub,
-                                                     right_image_sub], 1)
-        time_sync.registerCallback(self.analyze_images)
 
     def analyze_images(self, left_image, right_image):
         net_output = self.disp_net.eval([left_image, right_image])
@@ -27,4 +22,13 @@ class DispnetWrapper:
 
 
 if __name__ == '__main__':
+    rospy.init_node('dispnet_wrapper')
+    dispnet_wrapper = DispnetWrapper()
+
+    left_image_sub = message_filters.Subscriber('/stereo/left/image_raw', Image)
+    right_image_sub = message_filters.Subscriber('/stereo/right/image_raw', Image)
+    time_sync = message_filters.TimeSynchronizer([left_image_sub,
+                                                  right_image_sub], 1)
+
+    time_sync.registerCallback(dispnet_wrapper.analyze_images)
     rospy.spin()
